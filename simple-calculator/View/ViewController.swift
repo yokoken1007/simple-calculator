880d69725e2e629d281451821bb9419601f7de63
yokoyama <yokoyama@anysense.co.jp>
2022-07-06 12:28:26 +0900

    ‘初回アップロード’

---
+++ simple-calculator/View/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  simple-calculator
+//
+//  Created by 横山　賢一 on 2022/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var aaa:Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        aaa = 200
+        
+    }
+
+
+}
+
