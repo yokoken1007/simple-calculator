eaf01adc2480e67366b14aee5c2834a592fe2c1d
yokoyama <yokoyama@anysense.co.jp>
2022-07-06 14:21:10 +0900

    .gitignoreを反映させる

--- simple-calculator/View/ViewController.swift
+++ simple-calculator/View/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        aaa = 300
+        aaa = 400
         
     }
 
